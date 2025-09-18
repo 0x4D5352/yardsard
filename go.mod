8a13f822d895f1641337a435676ba982b94373f1
0x4D5352 <67082011+0x4D5352@users.noreply.github.com>
2025-09-18 08:28:42 -0500

    feat: add new chart library

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/0x4D5352/yardsard
 
 go 1.25.1
 
-require golang.org/x/text v0.29.0
+require (
+	github.com/guptarohit/asciigraph v0.7.3
+	github.com/vdobler/chart v1.0.0
+	golang.org/x/text v0.29.0
+)
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"image/color"
 	"math/rand/v2"
 	"os"
 	"time"
@@ -9,6 +10,10 @@ import (
 	"golang.org/x/text/language"
 	"golang.org/x/text/message"
 	"golang.org/x/text/number"
+
+	"github.com/guptarohit/asciigraph"
+	"github.com/vdobler/chart"
+	"github.com/vdobler/chart/txtg"
 )
 
 type SimulationState struct {
@@ -77,35 +82,109 @@ func (s *SimulationState) yardSaleIteration() {
 	}
 }
 
+func (s *SimulationState) printWealth(t string) {
+	floatWealth := make([]float64, len(s.PopWealth))
+	xAxis := make([]float64, len(s.PopWealth))
+	width := 300
+	height := 70
+	maxWealth := 0
+	for i, v := range s.PopWealth {
+		if v > maxWealth {
+			maxWealth = v
+		}
+		xAxis[i] = float64(i + 1)
+		floatWealth[i] = float64(v)
+	}
+	var yMax int
+	if maxWealth < s.TotalWealth/8 {
+		yMax = s.TotalWealth / 8
+	} else if maxWealth < s.TotalWealth/7 {
+		yMax = s.TotalWealth / 7
+	} else if maxWealth < s.TotalWealth/6 {
+		yMax = s.TotalWealth / 6
+	} else if maxWealth < s.TotalWealth/5 {
+		yMax = s.TotalWealth / 5
+	} else if maxWealth < s.TotalWealth/4 {
+		yMax = s.TotalWealth / 4
+	} else if maxWealth < s.TotalWealth/3 {
+		yMax = s.TotalWealth / 3
+	} else if maxWealth < s.TotalWealth/2 {
+		yMax = s.TotalWealth / 2
+	} else {
+		yMax = s.TotalWealth
+	}
+	// fmt.Println(len(xAxis))
+	// fmt.Println(len(floatWealth))
+	// xAxis = append(floatWealth, float64(len(s.PopWealth)+1))
+	// floatWealth = append(floatWealth, 0)
+	switch t {
+	case "asciigraph":
+		graph := asciigraph.Plot(floatWealth,
+			asciigraph.Width(width),
+			asciigraph.Height(height),
+			asciigraph.UpperBound(float64(s.TotalWealth)))
+		fmt.Println(graph)
+	case "chart":
+		wealth := chart.Style{Symbol: 'o',
+			LineColor: color.NRGBA{0x00, 0xcc, 0x00, 0xff},
+			FillColor: color.NRGBA{0x80, 0xff, 0x80, 0xff},
+			LineStyle: chart.SolidLine,
+			LineWidth: 1,
+		}
+		barc := chart.BarChart{
+			Title:        "Wealth Distribution",
+			SameBarWidth: true,
+		}
+		barc.Key.Hide = true
+		barc.XRange.ShowZero = true
+		barc.XRange.MaxMode = chart.RangeMode{Fixed: true, Value: float64(s.Population)}
+		barc.XRange.MinMode = chart.RangeMode{Fixed: true, Value: 0}
+		barc.YRange.ShowZero = true
+		barc.YRange.MaxMode = chart.RangeMode{Fixed: true, Value: float64(yMax)}
+		barc.YRange.MinMode = chart.RangeMode{Fixed: true, Value: 0}
+		barc.AddDataPair("Wealth", xAxis, floatWealth, wealth)
+		barc.XRange.TicSetting.Delta = 0
+		barc.YRange.TicSetting.Delta = 0
+		tgr := txtg.New(width, height)
+		barc.Plot(tgr)
+		fmt.Println(tgr.String() + "\n")
+	}
+
+}
+
 func initializeSimulation(people, plays, gain, loss, init int) *SimulationState {
 	s := SimulationState{
-		PopWealth:   make([]int, people),
-		PopIndices:  make([]int, people),
-		Population:  people,
-		Plays:       plays,
-		Gain:        gain,
-		Loss:        loss,
-		TotalWealth: init,
+		PopWealth:  make([]int, people),
+		PopIndices: make([]int, people),
+		Population: people,
+		Plays:      plays,
+		Gain:       gain,
+		Loss:       loss,
 	}
 	for i := range people {
 		s.PopIndices[i] = i
 		s.PopWealth[i] = rand.IntN(init) + 1
 		s.TotalWealth += s.PopWealth[i]
 	}
-	s.OligarchLimit = int(float64(s.TotalWealth) * 0.75)
+	for s.TotalWealth < init*people {
+		s.PopWealth[rand.IntN(people)] += 1
+		s.TotalWealth++
+	}
+	s.OligarchLimit = int(float64(s.TotalWealth) * 0.95)
 	return &s
 }
 
 func main() {
-	init := 1000
+	people := 10_000
+	init := 100
 	p := message.NewPrinter(language.AmericanEnglish)
-	s := initializeSimulation(100_000, 100, 20, 17, init)
+	s := initializeSimulation(people, 100, 20, 17, init)
 	fmt.Println("Intitial Conditions:")
 	p.Printf("Population Size: %d\n", s.Population)
 	p.Printf("Possible Max Starting Wealth: $%d\n", number.Decimal(init))
 	p.Printf("Total Available Wealth: $%d\n", s.TotalWealth)
 	p.Printf("Plays per round: %d\n", s.Plays)
-	p.Println("Note: one period (.) represents one round.")
+	// p.Println("Note: one period (.) represents one round.")
 	fmt.Printf("Starting simulation!")
 	startingWealth := make([]int, s.Population)
 	copy(startingWealth, s.PopWealth)
@@ -113,18 +192,32 @@ func main() {
 	j := 0
 	for {
 		// fmt.Printf("Starting round %d...\n", j)
-		if j%100 == 0 {
-			fmt.Println()
-		}
-		fmt.Printf(".")
-		// time.Sleep(1 * time.Millisecond)
+		// if j%100 == 0 {
+		// 	fmt.Println()
+		// }
+		// fmt.Printf(".")
+		time.Sleep(17 * time.Millisecond)
+		fmt.Printf("\033[2J\033[H")
+		p.Printf("Population Size: %d | ", s.Population)
+		p.Printf("Possible Max Starting Wealth: $%d | ", number.Decimal(init))
+		p.Printf("Total Available Wealth: $%d | ", s.TotalWealth)
+		p.Printf("Plays per round: %d\n", s.Plays)
+		fmt.Println()
+		s.printWealth("chart")
 		s.yardSaleIteration()
 		j++
 		// fmt.Println()
 		for i := range s.PopWealth {
 			if s.PopWealth[i] >= s.OligarchLimit {
-				fmt.Printf(".")
-				p.Printf("\nAfter %d rounds, Agent %d (started with $%d) has become an oligarch with $%d out of the available $%d!\n", number.Decimal((j+1)*s.Plays), i, number.Decimal(startingWealth[i]), number.Decimal(s.PopWealth[i]), number.Decimal(s.TotalWealth))
+				// fmt.Printf(".")
+				fmt.Printf("\033[2J\033[H")
+				p.Printf("\nAfter %d rounds, Agent %d (started with $%d) has become an oligarch with $%d out of the available $%d!\n\n",
+					number.Decimal((j+1)*s.Plays),
+					i,
+					number.Decimal(startingWealth[i]),
+					number.Decimal(s.PopWealth[i]),
+					number.Decimal(s.TotalWealth))
+				s.printWealth("chart")
 				os.Exit(0)
 			}
 		}
