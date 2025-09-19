9af6b08bcd4a77eefc55b23a6f71a1071d96d2fa
0x4D5352 <67082011+0x4D5352@users.noreply.github.com>
2025-09-19 17:15:21 -0500

    temp: push to remote, squash later

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"image/color"
 	"math/rand/v2"
 	"os"
+	"strconv"
 	"sync"
 	"time"
 
@@ -13,7 +15,6 @@ import (
 	"golang.org/x/text/message"
 	"golang.org/x/text/number"
 
-	"github.com/guptarohit/asciigraph"
 	"github.com/vdobler/chart"
 	"github.com/vdobler/chart/txtg"
 )
@@ -21,12 +22,15 @@ import (
 type SimulationState struct {
 	PopWealth     []int // an array of the current wealth for each agent
 	PopIndices    []int // an array of the indexes of Population, for Fisher-Yates
-	Population    int   // the number of agents in the simulation
-	Plays         int   // the number of rounds within an iteration step
-	Gain          int   // how much A gives to B, in percentage of B's wealth
-	Loss          int   // how much A takes from B, in percentage of B's wealth
-	TotalWealth   int   // how much money each agent in the simulation begins with
-	OligarchLimit int   // how much money an agent needs to be considered an oligarch
+	Now           time.Time
+	Population    int // the number of agents in the simulation
+	Plays         int // the number of rounds within an iteration step
+	Gain          int // how much A gives to B, in percentage of B's wealth
+	Loss          int // how much A takes from B, in percentage of B's wealth
+	TotalWealth   int // how much money each agent in the simulation begins with
+	OligarchLimit int // how much money an agent needs to be considered an oligarch
+	TermWidth     int
+	TermHeight    int
 }
 
 // Fisher-Yates is an algorithm that produces an unbiased random
@@ -90,15 +94,10 @@ func (s *SimulationState) yardSaleIteration() {
 	}
 }
 
-func (s *SimulationState) printWealth(t string) {
-	fd := int(os.Stdin.Fd())
-	width, height, err := term.GetSize(fd)
-	if err != nil {
-		fmt.Println("Error getting terminal size: ", err)
-		width = 80
-		height = 24
-	}
-	height = (height / 2) + (height / 4)
+func (s *SimulationState) printWealth() string {
+	// there's gotta be a better way to do this stuff
+	width := (s.TermWidth / 4) + (s.TermWidth / 6)
+	height := (s.TermHeight / 2) + (s.TermHeight / 4)
 	floatWealth := make([]float64, len(s.PopWealth))
 	xAxis := make([]float64, len(s.PopWealth))
 	maxWealth := 0
@@ -112,8 +111,6 @@ func (s *SimulationState) printWealth(t string) {
 	var yMax int
 	if maxWealth < s.TotalWealth/8 {
 		yMax = s.TotalWealth / 8
-	} else if maxWealth < s.TotalWealth/6 {
-		yMax = s.TotalWealth / 6
 	} else if maxWealth < s.TotalWealth/4 {
 		yMax = s.TotalWealth / 4
 	} else if maxWealth < s.TotalWealth/2 {
@@ -123,45 +120,48 @@ func (s *SimulationState) printWealth(t string) {
 	}
 	// fmt.Println(len(xAxis))
 	// fmt.Println(len(floatWealth))
-	// xAxis = append(floatWealth, float64(len(s.PopWealth)+1))
-	// floatWealth = append(floatWealth, 0)
-	switch t {
-	case "asciigraph":
-		graph := asciigraph.Plot(floatWealth,
-			asciigraph.Width(width),
-			asciigraph.Height(height),
-			asciigraph.UpperBound(float64(s.TotalWealth)))
-		fmt.Println(graph)
-	case "chart":
-		wealth := chart.Style{Symbol: '.',
-			LineColor: color.NRGBA{0x00, 0xcc, 0x00, 0xff},
-			FillColor: color.NRGBA{0x80, 0xff, 0x80, 0xff},
-			LineStyle: chart.SolidLine,
-			LineWidth: 1,
-		}
-		barc := chart.BarChart{
-			Title:        "Wealth Distribution",
-			SameBarWidth: false,
-			// ShowVal:      1,
-		}
-		barc.Key.Hide = true
-		barc.XRange.ShowZero = true
-		barc.XRange.MaxMode = chart.RangeMode{Fixed: true, Value: float64(s.Population)}
-		barc.XRange.MinMode = chart.RangeMode{Fixed: true, Value: 0}
-		barc.YRange.ShowZero = true
-		barc.YRange.MaxMode = chart.RangeMode{Fixed: true, Value: float64(yMax)}
-		barc.YRange.MinMode = chart.RangeMode{Fixed: true, Value: 0}
-		barc.AddDataPair("Wealth", xAxis, floatWealth, wealth)
-		barc.XRange.TicSetting.Delta = 0
-		barc.YRange.TicSetting.Delta = 0
-		tgr := txtg.New(width, height)
-		barc.Plot(tgr)
-		fmt.Println(tgr.String())
+	wealth := chart.Style{
+		Symbol:    'o',
+		LineColor: color.NRGBA{0xcc, 0x00, 0x00, 0xff},
+		FillColor: color.NRGBA{0xff, 0x80, 0x80, 0xff},
+		LineStyle: chart.SolidLine,
+		LineWidth: 1,
 	}
+	barc := chart.BarChart{
+		Title:        "Wealth Distribution",
+		SameBarWidth: true,
+		Stacked:      true,
+		// ShowVal:      1,
+	}
+	barc.Reset()
+	barc.Key.Hide = true
+	barc.XRange.ShowZero = true
+	barc.XRange.MaxMode = chart.RangeMode{Fixed: true, Value: float64(s.Population)}
+	barc.XRange.MinMode = chart.RangeMode{Fixed: true, Value: 0}
+	barc.YRange.ShowZero = true
+	// barc.YRange.MaxMode = chart.RangeMode{Fixed: true, Value: float64(s.TotalWealth)}
+	barc.YRange.MaxMode = chart.RangeMode{Fixed: true, Value: float64(yMax)}
+	barc.YRange.MinMode = chart.RangeMode{Fixed: true, Value: 0}
+	barc.AddDataPair("Wealth", xAxis, floatWealth, wealth)
+	barc.XRange.TicSetting.Delta = 0
+	barc.YRange.TicSetting.Delta = 0
+	tgr := txtg.New(width, height)
+	barc.Plot(tgr)
+	return tgr.String()
+}
 
+func (s *SimulationState) printHistogram() string {
+	return "to be implemented"
 }
 
 func initializeSimulation(people, plays, gain, loss, init int) *SimulationState {
+	fd := int(os.Stdin.Fd())
+	width, height, err := term.GetSize(fd)
+	if err != nil {
+		fmt.Println("Error getting terminal size: ", err)
+		width = 80
+		height = 24
+	}
 	s := SimulationState{
 		PopWealth:  make([]int, people),
 		PopIndices: make([]int, people),
@@ -169,6 +169,8 @@ func initializeSimulation(people, plays, gain, loss, init int) *SimulationState 
 		Plays:      plays,
 		Gain:       gain,
 		Loss:       loss,
+		TermWidth:  width,
+		TermHeight: height,
 	}
 	for i := range people {
 		s.PopIndices[i] = i
@@ -184,9 +186,43 @@ func initializeSimulation(people, plays, gain, loss, init int) *SimulationState 
 }
 
 func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("Welcome to yardsard, a Yard Sale Model Simulator!")
+	fmt.Printf("Please enter the number of people to simulate (default = 100):")
 	people := 100
+	var err error
+	if scanner.Scan() {
+		input := scanner.Text()
+		if input != "" {
+			people, err = strconv.Atoi(input)
+			if err != nil {
+				// this is all very ugly, forgive me
+				people = 100
+			}
+		}
+	}
+	if err = scanner.Err(); err != nil {
+		fmt.Printf("Error reading input: %v\n", err)
+	}
+	fmt.Printf("Please enter the amount of money each person should receive (default = $100):")
 	init := 100
+	if scanner.Scan() {
+		input := scanner.Text()
+		if input != "" {
+			init, err = strconv.Atoi(input)
+			if err != nil {
+				// this is all very ugly, forgive me
+				people = 100
+			}
+		}
+	}
+	if err = scanner.Err(); err != nil {
+		fmt.Printf("Error reading input: %v\n", err)
+	}
 	fps := (1000 / 100) * time.Millisecond
+	if people > 3_000 {
+		fps = 0 * time.Millisecond
+	}
 	p := message.NewPrinter(language.AmericanEnglish)
 	s := initializeSimulation(people, 100, 20, 17, init)
 	fmt.Println("Intitial Conditions:")
@@ -194,11 +230,16 @@ func main() {
 	p.Printf("Possible Max Starting Wealth: $%d\n", number.Decimal(init))
 	p.Printf("Total Available Wealth: $%d\n", s.TotalWealth)
 	p.Printf("Plays per round: %d\n", s.Plays)
+	fmt.Printf("TermInfo: Width = %d, Height = %d\n", s.TermWidth, s.TermHeight)
 	// p.Println("Note: one period (.) represents one round.")
-	fmt.Printf("Starting simulation!")
+	fmt.Printf("Press enter to start simulaton!")
 	startingWealth := make([]int, s.Population)
+	_, err = fmt.Scanln()
+	if err != nil {
+		fmt.Println(err)
+	}
 	copy(startingWealth, s.PopWealth)
-	time.Sleep(1 * time.Second)
+	s.Now = time.Now()
 	j := 0
 	for {
 		// fmt.Printf("Starting round %d...\n", j)
@@ -206,27 +247,36 @@ func main() {
 		// 	fmt.Println()
 		// }
 		// fmt.Printf(".")
-		time.Sleep(fps)
+		s.yardSaleIteration()
 		fmt.Printf("\033[2J\033[H")
 		p.Printf("Population Size: %d | ", s.Population)
 		p.Printf("Possible Max Starting Wealth: $%d | ", number.Decimal(init))
 		p.Printf("Total Available Wealth: $%d | ", s.TotalWealth)
-		p.Printf("Plays per round: %d\n", s.Plays)
-		s.yardSaleIteration()
-		s.printWealth("chart")
+		p.Printf("Plays per round: %d | ", s.Plays)
+		now := time.Now()
+		dur := now.Sub(s.Now)
+		s.Now = now
+		fmt.Printf("Elapsed time between iterations: %d milliseconds", dur.Milliseconds())
+		fmt.Println()
+		fmt.Println()
+		w := s.printWealth()
+		h := s.printHistogram()
+		fmt.Printf("%s     %s", w, h)
+		// time.Sleep(fps * 4)
+		time.Sleep(fps)
 		j++
 		// fmt.Println()
 		for i := range s.PopWealth {
 			if s.PopWealth[i] >= s.OligarchLimit {
 				// fmt.Printf(".")
 				fmt.Printf("\033[2J\033[H")
-				p.Printf("After %d rounds, Agent %d (started with $%d) has become an oligarch with $%d out of the available $%d!\n",
+				p.Printf("After %d rounds, Agent %d (started with $%d) has become an oligarch with $%d out of the available $%d!\n\n",
 					number.Decimal((j+1)*s.Plays),
 					i,
 					number.Decimal(startingWealth[i]),
 					number.Decimal(s.PopWealth[i]),
 					number.Decimal(s.TotalWealth))
-				s.printWealth("chart")
+				s.printWealth()
 				os.Exit(0)
 			}
 		}
